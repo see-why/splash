3800e26b3172e7d030f8ffc847eb12fbd4cb4ddc
Iyadi Cyril <iyadicy@gmail.com>
2024-07-07 13:49:40 +0100

    refactor: remove class method text_node_to_html_node

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,4 @@
-from htmlnode import HtmlNode, LeafNode, ParentNode
+from src.htmlnode import HtmlNode, LeafNode, ParentNode
 
 class TextNode:
   text_type_bold = "bold"
@@ -18,25 +18,5 @@ class TextNode:
              and self.text_type == other.text_type 
              and self.url == other.url)
   
-  def text_node_to_html_node(self):
-    match self.text_type:
-      case TextNode.text_type_text:
-        return LeafNode(None, self.text)
-      case TextNode.text_type_bold:
-        return LeafNode("b", self.text)
-      case TextNode.text_type_italic:
-        return LeafNode("i", self.text)
-      case TextNode.text_type_code:
-        return LeafNode("code", self.text)
-      case TextNode.text_type_link:
-        return LeafNode("a", self.text, {"href": self.url})
-      case TextNode.text_type_image:
-        return LeafNode("img", "", {"src": self.url, "alt": self.text})
-      
-      case _:
-        raise Exception("invalid text type")
-        
-
   def __repr__(self):
     return f"TextNode('{self.text}', '{self.text_type}', '{self.url}')"
-
