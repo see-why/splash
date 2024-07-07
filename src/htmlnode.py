4f64215db81027eabc081abe9048cbcf487782ec
Iyadi Cyril <iyadicy@gmail.com>
2024-07-07 13:46:23 +0100

    custome equality behaviour

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -5,6 +5,14 @@ class HtmlNode:
     self.children = children
     self.props = props
 
+  def __eq__(self, other):
+    return (
+      self.tag == other.tag and
+      self.value == other.value and 
+      self.children == other.children and 
+      self.props == other.props
+    )
+
   def to_html(self):
     raise NotImplementedError("to_html method not implemented")
   
