90fa1b62dc1c3d163acd8a34b3b07fe980c3e244
Iyadi Cyril <iyadicy@gmail.com>
2024-07-07 13:47:14 +0100

    extend: add more inline mark down formatting functions

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,5 @@
-from textnode import TextNode
+import re
+from src.textnode import TextNode
 
   # work on nested examples later  
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -23,4 +24,50 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
     new_nodes.extend(split_nodes)
 
+  return new_nodes
+
+def extract_markdown_images(text):
+  return re.findall(r'!\[(.*?)\]\((.*?)\)', text)
+
+def extract_markdown_links(text):
+  return re.findall(r'\[(.*?)\]\((.*?)\)', text)
+
+def split_nodes_link(old_nodes):
+  new_nodes = []
+  for node in old_nodes:
+    if node.text_type != TextNode.text_type_text:
+      new_nodes.append(node)
+      continue
+    link_tuples = extract_markdown_links(node.text)
+    text = node.text
+    for link_tuple in link_tuples:
+      split_nodes = text.split(f"[{link_tuple[0]}]({link_tuple[1]})", 1)
+      if split_nodes[0]:
+        new_nodes.append(TextNode(split_nodes[0], TextNode.text_type_text))
+      new_nodes.append(TextNode(link_tuple[0], TextNode.text_type_link, link_tuple[1]))
+      text = split_nodes[1]
+
+    if text:
+      new_nodes.append(TextNode(text, TextNode.text_type_text))
+
+  return new_nodes
+
+def split_nodes_image(old_nodes):
+  new_nodes = []
+  for node in old_nodes:
+    if node.text_type != TextNode.text_type_text:
+      new_nodes.append(node)
+      continue
+    image_tuples = extract_markdown_images(node.text)
+    text = node.text
+    for image_tuple in image_tuples:
+      split_nodes = text.split(f"![{image_tuple[0]}]({image_tuple[1]})", 1)
+      if split_nodes[0]:
+        new_nodes.append(TextNode(split_nodes[0], TextNode.text_type_text))
+      new_nodes.append(TextNode(image_tuple[0], TextNode.text_type_image, image_tuple[1]))
+      text = split_nodes[1]
+
+    if text:
+      new_nodes.append(TextNode(text, TextNode.text_type_text))
+    
   return new_nodes
