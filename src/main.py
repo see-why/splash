0b9c975ca9dbe575ddb009e42f1a66dba3e3401b
Iyadi Cyril <iyadicy@gmail.com>
2024-07-09 13:31:49 +0100

    extend: add copy directory functionality

--- src/main.py
+++ src/main.py
@@ -1,7 +1,33 @@
+import shutil
+import os
 from textnode import TextNode
 
 def main():
   text_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+  copy_content("static", "public")
   print(text_node)
+
+def copy_content(source_path, destination_path):
+  source_path_exists = os.path.exists(source_path)
+  destination_path_exists = os.path.exists(destination_path)
+
+  if not source_path_exists:
+    raise Exception(f"Source path {source_path} does not exist")
+
+  if destination_path_exists:
+    shutil.rmtree(destination_path)
+    
+  os.mkdir(destination_path)
+  
+  for dir in os.listdir(source_path):
+    current_path = os.path.join(source_path, dir)
+    new_destination_path =  os.path.join(destination_path, dir)
+    if os.path.isdir(current_path):
+      os.mkdir(new_destination_path)
+      copy_content(current_path, new_destination_path)
+    elif os.path.isfile(current_path):
+      path = shutil.copy(current_path, new_destination_path)
+      print(f"file: {current_path} copied to {path}")
+
 
 main()
